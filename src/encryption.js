16d562e9b8bb7cc3f6fcee33e791d4a1fd6bc306
astef021 <astefanescu021@gmail.com>
2022-07-02 10:31:31 -0400

    remove comments

--- src/encryption.js
+++ src/encryption.js
@@ -39,36 +39,3 @@ export const decryptData = (response) => {
   const data = crypto.privateDecrypt(privateKey, Buffer.from(response, 'base64'))
   return JSON.parse(data);
 }
-
-
-//generate encrypted privateKey
-// const {publicKey, privateKey } = crypto.generateKeyPairSync('rsa',
-//   {
-//     modulusLength: 4096,
-//     publicKeyEncoding: {
-//     type: 'spki',
-//     format: 'pem'
-//   },
-//   privateKeyEncoding: {
-//     type: 'pkcs8',
-//     format: 'pem',
-//     cipher: 'aes-256-cbc',
-//     passphrase: ''
-//   }
-// });
-
-// //generate key Object
-// const publickey = crypto.createPublicKey({
-//     key: publicKey,
-//     format: "pem",
-//     type: "pkcs1",
-//     encoding: "utf-8"
-// });
-
-// const privatekey = crypto.createPrivateKey({
-//     key: privateKey,
-//     format: "pem",
-//     type: "pkcs1",
-//     passphrase: "",
-//     encoding: "utf-8"
-// });
