f50a68eb3d4660ec13bcf19cfaf686357d9baab5
astef021 <astefanescu021@gmail.com>
2022-07-05 11:21:06 -0400

    fix importing module

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,6 @@
 import AppletModel from './applet.js';
 import ActivityModel from './activity.js';
-import ItemModel from './Item.js';
+import ItemModel from './item.js';
 
 export const Applet = AppletModel;
 export const Activity = ActivityModel;
