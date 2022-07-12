65041514733aa7f78a0c18609e2a15a09bfc7e8b
astef021 <astefanescu021@gmail.com>
2022-07-12 01:33:54 -0400

    update loading keys

--- src/encryption.js
+++ src/encryption.js
@@ -4,26 +4,35 @@ import fs from 'fs';
 
 const KEYS_FOLDER = process.env.KEYS_FOLDER;
 
-const privateKeyFile = fs.readFileSync(`${KEYS_FOLDER}/private.pem`);
-const privateKey = crypto.createPrivateKey({
-  key: privateKeyFile,
-  format: 'pem',
-  type: 'pkcs1',
-  passphrase: '',
-  encoding: 'utf-8'
-});
+const getPrivateKey = () => {
+  const file = fs.readFileSync(`${KEYS_FOLDER}/private.pem`);
+  const privateKey = crypto.createPrivateKey({
+    key: file,
+    format: 'pem',
+    type: 'pkcs1',
+    passphrase: '',
+    encoding: 'utf-8'
+  });
 
-const publicKeyFile = fs.readFileSync(`${KEYS_FOLDER}/public`);
-const publicKey = crypto.createPublicKey({
-  key: publicKeyFile,
-  format: 'pem',
-  type: 'pkcs1',
-  encoding: 'utf-8'
-});
+  return privateKey;
+}
+
+const getPublicKey = () => {
+  const file = fs.readFileSync(`${KEYS_FOLDER}/public`);
+  const publicKey = crypto.createPublicKey({
+    key: file,
+    format: 'pem',
+    type: 'pkcs1',
+    encoding: 'utf-8'
+  });
+
+  return publicKey;
+}
 
 export const verifyPublicKey = (key) => {
   try {
     const publicKey = crypto.createPublicKey({ key, format: 'pem', type: 'pkcs1', encoding: 'utf-8' });
+    const privateKey = getPrivateKey();
     const plain = crypto.privateDecrypt(privateKey, crypto.publicEncrypt(publicKey, 'pdf')).toString();
 
     return plain == 'pdf';
@@ -34,10 +43,12 @@ export const verifyPublicKey = (key) => {
 }
 
 export const encryptData = (data) => {
+  const publicKey = getPublicKey();
   return crypto.publicEncrypt(publicKey, JSON.stringify(data)).toString('base64');
 }
 
 export const decryptData = (response) => {
+  const privateKey = getPrivateKey();
   const data = crypto.privateDecrypt(privateKey, Buffer.from(response, 'base64'))
   return JSON.parse(data);
 }
