b9a2c1561ec9e914f8f85fd1d0cd49f42f6c6bac
karser <dpoddubny@gmail.com>
2023-08-07 12:11:32 +0300

    fix Request Entity is too large

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ const port = process.env.PORT || 3000;
 const outputsFolder = process.env.OUTPUTS_FOLDER || os.tmpdir();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 // app.use(authenticate);
 
 app.get('/', async (req: express.Request, res: express.Response) => {
