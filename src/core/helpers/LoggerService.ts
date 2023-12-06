8fb9462c610fd006148b41c5504124d5df06407a
Viktor Riabkov <vriabkov@scnsoft.com>
2023-12-06 16:40:29 +0200

    fix: /decrypt-user-responses error handler

--- src/core/helpers/LoggerService.ts
+++ src/core/helpers/LoggerService.ts
@@ -1,14 +1,14 @@
 class LoggerService {
   public info(message: string, ...args: unknown[]) {
-    console.info(message, args)
+    console.info(message, ...args)
   }
 
   public error(message: string, ...args: unknown[]) {
-    console.error(message, args)
+    console.error(message, ...args)
   }
 
   public warn(message: string, ...args: unknown[]) {
-    console.warn(message, args)
+    console.warn(message, ...args)
   }
 }
 
