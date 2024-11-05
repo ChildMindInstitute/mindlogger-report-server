f97d76852b1e7fe402cdb910e92ea347e7f58653
Kenroy Gobourne <kenroy.gobourne@gmail.com>
2024-11-04 22:30:45 -0500

    fix: Account for the way the mobile app represents number selections (M2-8174) (#72)
    Co-authored-by: Marty <mbanting@yahoo.com>

--- src/models/activity.ts
+++ src/models/activity.ts
@@ -122,14 +122,26 @@ export class ActivityEntity {
           const subscaleTableData = subscaleItem.subscaleTableData
 
           const subscaleTableDataItem = subscaleTableData.find(({ sex, age, rawScore }) => {
-            let reportedAge: number | null = null
+            let reportedAge: string | null = null
             if (typeof ageAnswer === 'string') {
-              reportedAge = Number(ageAnswer)
-            } else if ('value' in ageAnswer && typeof ageAnswer.value === 'number') {
-              reportedAge = ageAnswer.value
+              reportedAge = ageAnswer
+            } else if (ageAnswer && 'value' in ageAnswer && ['number', 'string'].includes(typeof ageAnswer.value)) {
+              reportedAge = String(ageAnswer.value)
             }
 
-            const withAge = age === reportedAge
+            const hasAgeInterval = age && typeof age === 'string' && age.includes(INTERVAL_SYMBOL)
+            let withAge = true
+
+            if (age) {
+              if (!hasAgeInterval) {
+                withAge = String(age) === reportedAge
+              } else {
+                const [minAge, maxAge] = age.replace(/\s/g, '').split(INTERVAL_SYMBOL)
+                const reportedAgeNum = Number(reportedAge)
+                withAge = Number(minAge) <= reportedAgeNum && reportedAgeNum <= Number(maxAge)
+              }
+            }
+
             const withSex = parseSex(sex) === String(genderAnswer?.value)
 
             if (!withSex || !withAge) return false
