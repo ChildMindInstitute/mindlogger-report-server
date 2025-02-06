2a1ba408a72c180a553245a10c67c83adad859d8
Rodrigo Merlo <rodrigo.merlo@metalab.com>
2025-02-06 18:30:01 -0300

    fix: Add flag to puppeteer for disable crashpad
    Included '--disable-crashpad-for-testing' in puppeteer launch args to  preventing crash reports.

--- src/pdf-utils.ts
+++ src/pdf-utils.ts
@@ -10,7 +10,7 @@ import { decryptData } from './modules/report/services/kmsEncryption'
 
 export const convertHtmlToPdf = async (html: string, saveTo: string): Promise<void> => {
   const browser = await puppeteer.launch({
-    args: ['--disable-dev-shm-usage', '--no-sandbox', '--headless', '--disable-gpu'],
+    args: ['--disable-dev-shm-usage', '--no-sandbox', '--headless', '--disable-gpu', '--disable-crashpad-for-testing'],
   })
 
   const page = await browser.newPage()
