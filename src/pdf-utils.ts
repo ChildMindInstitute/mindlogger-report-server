68f1ea40f5d99d48363390129f8e54df8290debc
Rodrigo Merlo <rodrigo.merlo@metalab.com>
2025-02-19 17:33:50 -0300

    fix: wait for images to load, before generate pdf

--- src/pdf-utils.ts
+++ src/pdf-utils.ts
@@ -14,7 +14,7 @@ export const convertHtmlToPdf = async (html: string, saveTo: string): Promise<vo
   })
 
   const page = await browser.newPage()
-  await page.setContent(html)
+  await page.setContent(html, { waitUntil: 'networkidle0' })
 
   try {
     createDirectoryIfNotExists(saveTo)
