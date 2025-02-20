3067dbe574f1e07e9c02060790b595b1c5efa033
Andrew Weiland <aweiland@users.noreply.github.com>
2025-02-20 11:04:51 -0500

    chore: Linking logs to traces and access logs

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,14 @@ import cors from 'cors'
 import { serverController } from './modules/server/server.controller'
 import { reportController } from './modules/report/report.controller'
 import { logger } from './core/services/LoggerService'
+import { logger as expressLogger } from 'express-winston'
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
+app.use(expressLogger({ winstonInstance: logger }))
 
 app.get('/', async (req: express.Request, res: express.Response) => {
   const buildVersion = process.env.BUILD_VERSION
