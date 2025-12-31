2cc9f1103251bbe1da10fc9923269cd1b96d1cb4
Chris Lei <chris.lei@childmind.org>
2025-12-31 10:37:30 -0800

    npx @eslint/migrate-config .eslintrc.js

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+const {
+    defineConfig,
+    globalIgnores,
+} = require("eslint/config");
+
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslintEslintPlugin = require("@typescript-eslint/eslint-plugin");
+const unusedImports = require("eslint-plugin-unused-imports");
+const globals = require("globals");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([{
+    languageOptions: {
+        parser: tsParser,
+        sourceType: "module",
+
+        parserOptions: {
+            project: "tsconfig.json",
+        },
+
+        globals: {
+            ...globals.node,
+            ...globals.jest,
+        },
+    },
+
+    plugins: {
+        "@typescript-eslint": typescriptEslintEslintPlugin,
+        "unused-imports": unusedImports,
+    },
+
+    extends: compat.extends("plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"),
+
+    rules: {
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "unused-imports/no-unused-imports": "error",
+
+        "no-console": ["error", {
+            allow: ["warn", "error", "info"],
+        }],
+    },
+}, globalIgnores(["**/.eslintrc.js"])]);
